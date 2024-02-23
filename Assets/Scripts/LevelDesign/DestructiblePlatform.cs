e43d673fec5c05ce29e395c1257488593648984c
SaintZoey <zoethu@hotmail.fr>
2024-02-23 18:57:28 +0100

    Initial commit

---
+++ Assets/Scripts/LevelDesign/DestructiblePlatform.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructiblePlatform : MonoBehaviour
+{
+    public float ExplosionForce = 10;
+    public float ExplosionRadius = 10;
+    public Transform explosionSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(explosionSource.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Destroy(Vector3 positionExplosion)
+    {
+        Rigidbody[] Bodies = GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody b in Bodies)
+        {
+            b.isKinematic = false;
+            b.useGravity = true;
+            b.AddExplosionForce(ExplosionForce, positionExplosion, ExplosionRadius);
+        }
+    }
+}
